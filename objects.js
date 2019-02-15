5e75c44ba64e96263a3846fe006c17b9f6bde5de
Adam Wolf <adamjwolf1@gmail.com>
2019-02-15 18:36:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = ({Tom_Petty: 'The Waiting'})
 
 function updatePlaylist(playlist, name, song) {
   
-  return Object.assign({playlist}, obj, { [Elvis: 'Houndog']})
+  return Object.assign({playlist}, obj, { [Elvis: 'Houndog'] })
 }
